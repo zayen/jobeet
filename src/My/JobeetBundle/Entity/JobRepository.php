f684a631c9695375e8c39ac24c944e045b2fa7db
Youssfi Sebti <youssfi.sebti@yahoo.fr>
2014-11-24 08:09:17 +0000

    affichage des categories avec son offres liées

--- src/My/JobeetBundle/Entity/JobRepository.php
+++ src/My/JobeetBundle/Entity/JobRepository.php
@@ -12,16 +12,33 @@ use Doctrine\ORM\EntityRepository;
 
 class JobRepository extends EntityRepository
 {
-    public function getList($page=1, $maxperpage=10)
+    public function getList($page = 1, $maxperpage = 10)
     {
         $q = $this->_em->createQueryBuilder()
             ->select('article')
-            ->from('JobeetBundle:Job','job')
-        ;
+            ->from('JobeetBundle:Job', 'job');
 
-        $q->setFirstResult(($page-1) * $maxperpage)
+        $q->setFirstResult(($page - 1) * $maxperpage)
             ->setMaxResults($maxperpage);
 
         return new Paginator($q);
+    }
+
+    public function getActiveJobs($category_id = null)
+    {
+        $qb = $this->createQueryBuilder('j')
+            ->where('j.expires_at > :date')
+            ->setParameter('date', date('Y-m-d H:i:s', time()))
+            ->orderBy('j.expires_at', 'DESC');
+
+        if($category_id)
+        {
+            $qb->andWhere('j.category = :category_id')
+                ->setParameter('category_id', $category_id);
+        }
+
+        $query = $qb->getQuery();
+
+        return $query->getResult();
     }
 }
