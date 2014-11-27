a74a1ca12eb50622409aede6662c1f0d702b352b
Youssfi Sebti <youssfi.sebti@yahoo.fr>
2014-11-27 09:28:38 +0000

    instalation admin bundle creer jobadmin et Postadmin

--- src/My/JobeetBundle/Admin/JobAdmin.php
+++ src/My/JobeetBundle/Admin/JobAdmin.php
@@ -1,38 +1,22 @@
 <?php
 
-
 namespace My\JobeetBundle\Admin;
 
 use Sonata\AdminBundle\Admin\Admin;
-use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 class JobAdmin extends Admin
 {
-    // Fields to be shown on create/edit forms
-    protected function configureFormFields(FormMapper $formMapper)
-    {
-        $formMapper
-            ->add('type')
-            ->add('company')
-            ->add('url')
-            ->add('position')
-            ->add('location')
-            ->add('description')
-            ->add('email')
-            ->add('expires_at')
-            ->add('created_at')
-            ->add('updated_at')
-            ->add('category', 'entity', array('class' => 'My\JobeetBundle\Entity\Category'))
-
-        ;
-    }
-
-    // Fields to be shown on filter forms
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper
+            ->add('id')
             ->add('type')
             ->add('company')
             ->add('url')
@@ -43,15 +27,16 @@ class JobAdmin extends Admin
             ->add('expires_at')
             ->add('created_at')
             ->add('updated_at')
-            ->add('category', 'entity', array('class' => 'My\JobeetBundle\Entity\Category'))
-
         ;
     }
 
-    // Fields to be shown on lists
+    /**
+     * @param ListMapper $listMapper
+     */
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
+            ->add('id')
             ->add('type')
             ->add('company')
             ->add('url')
@@ -62,8 +47,53 @@ class JobAdmin extends Admin
             ->add('expires_at')
             ->add('created_at')
             ->add('updated_at')
-            ->add('category', 'entity', array('class' => 'My\JobeetBundle\Entity\Category'))
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
 
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('type')
+            ->add('company')
+            ->add('url')
+            ->add('position')
+            ->add('location')
+            ->add('description')
+            ->add('email')
+            ->add('expires_at')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('type')
+            ->add('company')
+            ->add('url')
+            ->add('position')
+            ->add('location')
+            ->add('description')
+            ->add('email')
+            ->add('expires_at')
+            ->add('created_at')
+            ->add('updated_at')
         ;
     }
 }
---
+++ src/My/JobeetBundle/Controller/JobAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace My\JobeetBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class JobAdminController extends CRUDController
+{
+
+}
